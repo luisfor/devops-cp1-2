8550cc103cb4eda4e51aebd93ad6e2e528395a81
Luis Canedo <luillys83@yahoo.es>
2026-02-25 14:58:51 -0500

    test: anadir pruebas de division por cero y comprobacion estricta de tipos para 100% de cobertura

--- test/unit/calc_test.py
+++ test/unit/calc_test.py
@@ -40,6 +40,19 @@ class TestCalculate(unittest.TestCase):
         self.assertRaises(TypeError, self.calc.divide, 2, "2")
         self.assertRaises(TypeError, self.calc.divide, "2", "2")
 
+    def test_divide_method_fails_with_division_by_zero(self):
+        self.assertRaises(TypeError, self.calc.divide, 2, 0)
+        self.assertRaises(TypeError, self.calc.divide, 2.0, 0)
+
+    def test_check_types_fails_with_invalid_types(self):
+        self.assertRaises(TypeError, self.calc.check_types, "2", 2)
+        self.assertRaises(TypeError, self.calc.check_types, 2, "2")
+        self.assertRaises(TypeError, self.calc.check_types, "2", "2")
+        self.assertRaises(TypeError, self.calc.check_types, None, 2)
+        self.assertRaises(TypeError, self.calc.check_types, 2, None)
+        self.assertRaises(TypeError, self.calc.check_types, object(), 2)
+        self.assertRaises(TypeError, self.calc.check_types, 2, object())
+
     def test_multiply_method_returns_correct_result(self):
         self.assertEqual(4, self.calc.multiply(2, 2))
         self.assertEqual(0, self.calc.multiply(1, 0))
